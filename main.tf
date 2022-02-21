16b5f2ba733ebe9eacdb30d43d72a94fd132ae32
Oleh Ilnytskyi <oleh.ilnytskyi@rightmove.co.uk>
2022-02-21 16:00:32 +0000

    Added lambda logic in terraform

--- main.tf
+++ main.tf
@@ -28,20 +28,50 @@ resource "random_pet" "lambda_bucket_name" {
 
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket = random_pet.lambda_bucket_name.id
-
   #acl           = "private"
   force_destroy = true
-}
-
-data "archive_file" "lambda_hello_world" {
-  type       = "zip"
-  source_dir = "${path.module}/src/main/java/example/"
-  output_path = "${path.module}/build/distributions/spotify_backend_with_lambda.zip"
 }
 
 resource "aws_s3_object" "lambda_hello_world" {
   bucket = aws_s3_bucket.lambda_bucket.id
   key    = "spotify_backend_with_lambda.zip"
-  source = data.archive_file.lambda_hello_world.output_path
-  etag   = filemd5(data.archive_file.lambda_hello_world.output_path)
+  source = "${path.module}/build/distributions/spotify_backend_with_lambda.zip"
+  etag   = filemd5("${path.module}/build/distributions/spotify_backend_with_lambda.zip")
+}
+
+resource "aws_lambda_function" "hello_world" {
+  function_name    = "SpotifyBackend"
+  s3_bucket        = aws_s3_bucket.lambda_bucket.id
+  s3_key           = aws_s3_object.lambda_hello_world.key
+  runtime          = "java11"
+  handler          = "example.Handler"
+  source_code_hash = filebase64sha256("${path.module}/build/distributions/spotify_backend_with_lambda.zip")
+  role             = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "hello_world" {
+  name              = "/aws/lambda/${aws_lambda_function.hello_world.function_name}"
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name               = "serverless_lambda"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
